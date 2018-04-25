d638021ed9abeee1d5614b3a7caed216a8f1425f
Mark Laane <MarkLaane@gmail.com>
2018-04-25 11:52:32 +0300

    fix thrown when user.rev is missing

--- src/UserForm.js
+++ src/UserForm.js
@@ -47,7 +47,7 @@ const UserForm = ({ user, onSubmit }) => (
     <Header as="h2">
       {user == null ? 'New User' : `User: ${user.firstName} ${user.lastName}`}
     </Header>
-    <Label basic>Revision: {user._rev.split('-')[0]}</Label>
+    <Label basic>Revision: {user._rev && user._rev.split('-')[0]}</Label>
     <Formik
       enableReinitialize={true}
       initialValues={
