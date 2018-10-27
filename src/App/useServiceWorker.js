f6e7506f1edfabfb78686758804af77a38de2e4b
Mark Laane <MarkLaane@gmail.com>
2018-10-28 00:35:56 +0200

    beforeInstallPromptEvent was always reset to "null" in the hook. Place beforeInstallPromptEvent into the state of a component.

--- src/App/useServiceWorker.js
+++ src/App/useServiceWorker.js
@@ -4,14 +4,15 @@ import * as serviceWorker from '../registerServiceWorker';
 export default function useServiceWorker() {
   const [installing, setInstalling] = useState(false);
   const [installable, setInstallable] = useState(false);
-
-  let beforeInstallPromptEvent = null;
+  const [beforeInstallPromptEvent, setBeforeInstallPromptEvent] = useState(
+    null
+  );
 
   useEffect(() => {
     serviceWorker.register({
       onBeforeInstallPrompt: e => {
         console.log(e.platforms);
-        beforeInstallPromptEvent = e;
+        setBeforeInstallPromptEvent(e);
         setInstallable(true);
         console.log('App is ready to be installed on home screen!');
       }
@@ -23,16 +24,16 @@ export default function useServiceWorker() {
       console.log('App can not be installed - try again later');
       return;
     }
+    setInstallable(false);
     setInstalling(true);
     await beforeInstallPromptEvent.prompt();
     const choiceResult = await beforeInstallPromptEvent.userChoice;
-    setInstallable(false);
     if (choiceResult.outcome === 'accepted') {
       console.log('User accepted the A2HS prompt');
     } else {
       console.log('User dismissed the A2HS prompt');
     }
-    beforeInstallPromptEvent = null;
+    setBeforeInstallPromptEvent(null);
     setInstalling(false);
   };
 
